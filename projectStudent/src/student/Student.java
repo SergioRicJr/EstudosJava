3309d91d9843f5a37f13c7398f7af6c39f3dba5f
Sergio <sergioricjr7@gmail.com>
2023-03-15 17:51:32 -0300

    projeto estudante

---
+++ projectStudent/src/student/Student.java
@@ -0,0 +1,17 @@
+package student;
+
+public class Student {
+	private String name;
+	private int media;
+	private int studentId;
+	private String ssn = "n";
+	private double gpa;
+	
+	public Student(int studentId, String name, String ssn) {
+		this.name = name;
+		this.studentId = studentId;
+		this.ssn = ssn;
+	}
+	// @override faz que metodo possa ser sobrescrevido sem problemas
+	// podendo permitir que objeto seja instanciado com ou sem parametros definidos
+}
---
+++ projectStudent/src/student/StudentTester.java
@@ -0,0 +1,9 @@
+package student;
+
+public class StudentTester {
+	
+	public static void main(String[] args) {
+		Student st1 = new Student(1, "Juninho", "xxxxxx");
+		System.out.println(st1);
+	}
+}
